de4bd4d6b785f75c9655589d5ef924fd7361b616
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-07-26 16:51:24 -0300

    fix(erc20_mint): update erc20 mint example

--- examples/erc20_mint/index.ts
+++ examples/erc20_mint/index.ts
@@ -1,4 +1,6 @@
 import { GetDataByRID, JSON, Log, SendTx } from "@w3bstream/wasm-sdk";
+import { ethToHex, buildTxSlot, buildTxString } from "@w3bstream/wasm-sdk/assembly/utility";
+
 export { alloc } from "@w3bstream/wasm-sdk";
 
 export function start(rid: i32): i32 {
@@ -15,16 +17,17 @@ export function start(rid: i32): i32 {
   let chainIdOrNull: JSON.Integer | null = jsonObj.getInteger("chainId");
   let ERC20Address: JSON.Str | null = jsonObj.getString("address");
   let MintTo: JSON.Str | null = jsonObj.getString("mintTo");
+
   if (chainIdOrNull && ERC20Address && MintTo) {
-    const res = SendTx(
-      chainIdOrNull.valueOf() as i32,
-      ERC20Address.valueOf(),
-      "0",
-      `0x40c10f19000000000000000000000000${MintTo.valueOf().replace(
-        "0x",
-        ""
-      )}0000000000000000000000000000000000000000000000000de0b6b3a7640000`
+    const amountToMint = "10";
+
+    const res = mintTokens(
+      chainIdOrNull.valueOf() as i32, 
+      ERC20Address.valueOf(), 
+      MintTo.valueOf(), 
+      amountToMint
     );
+
     Log("wasm send tx result:" + res);
   } else {
     Log(`params invalid`);
@@ -32,10 +35,37 @@ export function start(rid: i32): i32 {
   return 0;
 }
 
-
 export function abort(
   message: string | null,
   fileName: string | null,
   lineNumber: u32,
   columnNumber: u32
 ): void { }
+
+function mintTokens<T>(chainId: i32, contractAddress: string, recipient: string, amount: T): string {
+  const MINT_FUNCTION_ADDR = "40c10f19";
+  const txData = buildTxData(MINT_FUNCTION_ADDR, recipient, amount);
+
+  return SendTx(
+    chainId,
+    contractAddress,
+    "0",
+    txData
+  );
+}
+
+function buildTxData<T>(
+  functionAddr: string,
+  recipient: string,
+  tokenAmount: T
+): string {
+  const slotForRecipient = buildTxSlot(recipient.replace("0x", ""));
+  const slotForAmount = tokenNumberToTxSlot(tokenAmount);
+
+  return buildTxString([functionAddr, slotForRecipient, slotForAmount]);
+}
+
+function tokenNumberToTxSlot<T>(value: T): string {
+  const ethHex = ethToHex(value);
+  return buildTxSlot(ethHex);
+}
